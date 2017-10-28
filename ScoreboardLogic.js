1220363f4d72614d60871b790639a569f6e8bc1e
Megawats777 <jedsemilla@gmail.com>
2017-10-28 13:32:34 -0400

    Added reset scores button to scoreboard, and transfered score display update statements into one method

--- ScoreboardLogic.js
+++ ScoreboardLogic.js
@@ -18,11 +18,10 @@ var team2ScoreText;
 // Element 1 = Team 2 button
 var scoreIncreaseButtons = new Array(2);
 var scoreDecreaseButtons = new Array(2);
-
+var resetScoreValueButton;
 
 // Initialize the page
-function initialize()
-{
+function initialize() {
     // Get document references
     getDocumentReferences();
 
@@ -31,8 +30,8 @@ function initialize()
 }
 
 // Get document references
-function getDocumentReferences()
-{
+function getDocumentReferences() {
+
     team1ScoreText = document.getElementById("Team1Score");
     team2ScoreText = document.getElementById("Team2Score");
 
@@ -42,143 +41,161 @@ function getDocumentReferences()
 
     scoreDecreaseButtons[0] = document.getElementById("Team1ScoreDecreaseButton");
     scoreDecreaseButtons[1] = document.getElementById("Team2ScoreDecreaseButton");
+
+    resetScoreValueButton = document.getElementById("ResetScoreValueButton");
 }
 
 // Bind function calls
-function bindFunctionCalls()
-{
-    // Bind functions to the score adjustment buttons
-    scoreIncreaseButtons[0].addEventListener("click", function(){ increaseTeamScoreValues(1); });
-    scoreIncreaseButtons[1].addEventListener("click", function(){ increaseTeamScoreValues(2); });
+function bindFunctionCalls() {
 
-    scoreDecreaseButtons[0].addEventListener("click", function(){ decreaseTeamScoreValues(1); });
-    scoreDecreaseButtons[1].addEventListener("click", function(){ decreaseTeamScoreValues(2); });
+    // Bind functions to the score adjustment buttons
+    scoreIncreaseButtons[0].addEventListener("click", function () { increaseTeamScoreValues(1); });
+    scoreIncreaseButtons[1].addEventListener("click", function () { increaseTeamScoreValues(2); });
+
+    scoreDecreaseButtons[0].addEventListener("click", function () { decreaseTeamScoreValues(1); });
+    scoreDecreaseButtons[1].addEventListener("click", function () { decreaseTeamScoreValues(2); });
+
+    resetScoreValueButton.addEventListener("click", function () { resetTeamScoreValues(); });
 }
 
 // Increase team score values
 // PARAM 1: The selected team
-function increaseTeamScoreValues(teamSelected)
-{
-    if (teamSelected === 1)
-    {
+function increaseTeamScoreValues(teamSelected) {
+    
+    if (teamSelected === 1) {
+        
         // If the team 1 value is less than 999
         // Increase the team 1 score value
-        if (team1Score < 999)
-        {
-            if ((team1Score + teamScoreIncreaseInterval) >= 999)
-            {
+        if (team1Score < 999) {
+            if ((team1Score + teamScoreIncreaseInterval) >= 999) {
                 team1Score = 999;
             }
 
-            else
-            {
+            else {
                 team1Score += teamScoreIncreaseInterval;
             }
         }
-        
+
         // If the team 1 score value is equal or greater than 999
         // Set the team 1 score value to be 999
-        else if (team1Score >= 999)
-        {
+        else if (team1Score >= 999) {
             team1Score = 999;
-        } 
+        }
 
-        team1ScoreText.innerHTML = team1Score;
+        updateTeamScoreDisplayElements(1);
     }
 
-    else if (teamSelected === 2)
-    {
+    else if (teamSelected === 2) {
+        
         // If the team 2 value is less than 999
         // Increase the team 2 score value
-        if (team2Score < 999)
-        {
-            if ((team2Score + teamScoreIncreaseInterval) >= 999)
-            {
+        if (team2Score < 999) {
+            if ((team2Score + teamScoreIncreaseInterval) >= 999) {
                 team2Score = 999;
             }
-            
-            else
-            {
+
+            else {
                 team2Score += teamScoreIncreaseInterval;
             }
         }
 
         // If the team 2 score value is equal or greater than 999
         // Set the team 2 score value to be 999
-        else if (team2Score >= 999)
-        {
+        else if (team2Score >= 999) {
             team2Score = 999;
-        } 
+        }
 
-        team2ScoreText.innerHTML = team2Score;
+        updateTeamScoreDisplayElements(2);
     }
 
-    else
-    {
+    else {
         console.log("Invalid team selected");
     }
 }
 
 // Decrease team score values
 // PARAM 1: The selected team
-function decreaseTeamScoreValues(teamSelected)
-{
-    if (teamSelected === 1)
-    {
+function decreaseTeamScoreValues(teamSelected) {
+    
+    if (teamSelected === 1) {
+        
         // If the team 1 score value is greater than -999
         // Decrease the team 1 score value
-        if (team1Score > -999)
-        {
-            if ((team1Score - teamScoreDecreaseInterval) <= -999)
-            {
+        if (team1Score > -999) {
+            if ((team1Score - teamScoreDecreaseInterval) <= -999) {
                 team1Score = -999;
             }
 
-            else
-            {
+            else {
                 team1Score -= teamScoreDecreaseInterval;
             }
         }
 
         // If the team 1 score value is equal or less than -999
         // Set the team 1 score value to be -999
-        else if (team1Score <= -999)
-        {
+        else if (team1Score <= -999) {
             team1Score = -999;
         }
 
-        team1ScoreText.innerHTML = team1Score;
+        updateTeamScoreDisplayElements(1);
     }
 
-    else if (teamSelected === 2)
-    {
+    else if (teamSelected === 2) {
+        
         // If the team 2 score value is greater than -999
         // Decrease the team 2 score value
-        if (team2Score > -999)
-        {
-            if ((team2Score - teamScoreDecreaseInterval) <= -999)
-            {
+        if (team2Score > -999) {
+            if ((team2Score - teamScoreDecreaseInterval) <= -999) {
                 team2Score = -999;
             }
 
-            else
-            {
+            else {
                 team2Score -= teamScoreDecreaseInterval;
             }
         }
 
         // If the team 2 score value is equal or less than -999
         // Set the team 2 score value to be -999
-        else if (team2Score <= -999)
-        {
+        else if (team2Score <= -999) {
             team2Score = -999;
         }
 
-        team2ScoreText.innerHTML = team2Score;
+        updateTeamScoreDisplayElements(2);
     }
 
-    else
-    {
+    else {
         console.log("Invalid team selected");
+    }
+}
+
+// Set the score value of both teams to be 0
+function resetTeamScoreValues() {
+
+    team1Score = 0;
+    team2Score = 0;
+
+    updateTeamScoreDisplayElements(1);
+    updateTeamScoreDisplayElements(2);
+}
+
+// Update team score display elements
+// PARAM 1: The team display element to update
+function updateTeamScoreDisplayElements(selectedTeam) {
+    
+    // Depending on the selected team
+    // Update their display elements
+    switch (selectedTeam) {
+
+        case 1:
+            team1ScoreText.innerHTML = team1Score;
+            break;
+
+        case 2:
+            team2ScoreText.innerHTML = team2Score;
+            break;
+
+        default:
+            console.log("Invalid Team Selected");
+            break;
     }
 }
