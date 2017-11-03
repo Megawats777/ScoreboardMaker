45def6dd60010d30e72f2128e902693ceae34460
Megawats777 <jedsemilla@gmail.com>
2017-11-03 13:42:00 -0400

    Added ability to change the font of the scoreboard title

--- EditorLogic.js
+++ EditorLogic.js
@@ -23,6 +23,12 @@ var EDITOR_OswaldFontID = "Oswald, sans-serif";
 var EDITOR_OpenSansFontID = "Open Sans, sans-serif";
 var EDITOR_SairaSemiCondensedFontID = "Saira Semi Condensed, sans-serif";
 
+// Font dropdown controls
+var EDITOR_scoreboardTitleFontControl;
+var EDITOR_team1FontControl;
+var EDITOR_team2FontControl;
+var EDITOR_scoreFontControl;
+
 
 /*--Font Control Properties End--*/
 
@@ -51,6 +57,12 @@ function EDITOR_getDocumentReferences() {
     EDITOR_scoreboardTitleDisplay = document.getElementById("ScoreboardTitle");
     EDITOR_team1TitleDisplay = document.getElementById("Team1Title");
     EDITOR_team2TitleDisplay = document.getElementById("Team2Title");
+
+    // Get the font control elements
+    EDITOR_scoreboardTitleFontControl = document.getElementById("ScoreboardTitleFontControl");
+    EDITOR_scoreFontControl = document.getElementById("ScoreFontControl");
+    EDITOR_team1FontControl = document.getElementById("team1FontControl");
+    EDITOR_team2FontControl = document.getElementById("team2FontControl");
 }
 
 // Set the default values for the controls
@@ -69,6 +81,10 @@ function EDITOR_bindFunctionCalls() {
     EDITOR_scoreboardTitleInput.oninput = function () { EDITOR_applyTitleDisplayChanges(); };
     EDITOR_team1TitleInput.oninput = function () { EDITOR_applyTitleDisplayChanges(); };
     EDITOR_team2TitleInput.oninput = function () { EDITOR_applyTitleDisplayChanges(); };
+
+    // Bind functions to the font controls
+    EDITOR_scoreboardTitleFontControl.onchange = function () { EDITOR_applyFontChanges(); };
+
 }
 
 // Apply changes to title display elements
@@ -77,4 +93,25 @@ function EDITOR_applyTitleDisplayChanges() {
     EDITOR_scoreboardTitleDisplay.innerHTML = EDITOR_scoreboardTitleInput.value;
     EDITOR_team1TitleDisplay.innerHTML = EDITOR_team1TitleInput.value;
     EDITOR_team2TitleDisplay.innerHTML = EDITOR_team2TitleInput.value;
+}
+
+// Apply font changes
+function EDITOR_applyFontChanges() {
+
+    // Check for what font was selected for the scoreboard title
+    // Depending on the selected option set the font of that title
+    switch (EDITOR_scoreboardTitleFontControl.selectedIndex) {
+        
+        case 0:
+            EDITOR_scoreboardTitleDisplay.style.fontFamily = "Oswald, sans-serif";
+            break;
+
+        case 1:
+            EDITOR_scoreboardTitleDisplay.style.fontFamily = "Open Sans, sans-serif";
+            break;
+
+        case 2:
+            EDITOR_scoreboardTitleDisplay.style.fontFamily = "Saira Semi Condensed, sans-serif";
+            break;
+    }
 }
